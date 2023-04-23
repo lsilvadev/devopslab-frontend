fd7fb1dc59ceb15d8fa7052d58185fa85b9dfa16
Vic Corotto <victoria.corotto@unico.io>
2023-04-23 20:19:59 -0300

    add home and list components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-// Importing modules
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
+import HomeComponent from "./components/home";
 
 function App() {
-   	const [data, setData] = useState([]);
-
-	useEffect(() => {
-		fetch("$BACKEND_URL")
-		 .then((res) => res.json())
-      		 .then((result) => setData(result))
-      		 .catch((err) => console.log("error"));
-	}, []);
   return (
-    <div class="divTable">
-      {data &&
-        data.map((element, index) => (
-        <div class="headRow">
-          <div class="divCell" align="center">{element.song_id}</div>
-          <div class="divCell divCellName">{element.title}</div>
-          <div class="divCell divCellName">{element.artist}</div>
-          <div class="divCell" align="center">{element.genre}</div>
-          </div>
-        ))}
-    </div>
+    <HomeComponent />
   );
 
 }
