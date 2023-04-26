6fae496869dec10fdc8bfce1e8fd14c682dded92
Gabryelle Silva Santos <gabryelle-gaby@hotmail.com>
2023-04-26 16:35:52 -0300

    feat: add colors
    Resolves: add-modal

--- src/defaults/Colors.js
+++ src/defaults/Colors.js
@@ -1,3 +1,5 @@
 export const PRIMARY_COLOR= '#FF637D';
 export const WHITE = '#ffffff';
 export const DARK_PRIMARY_COLOR = '#FF4F6C';
+
+export const FONT_COLOR = '#353D58';
---
+++ src/utility/getRuleFormItem.js
@@ -0,0 +1,4 @@
+export const getRuleFormItem = (text) => ({
+  required: true,
+  message: `Por favor, ${text}.`
+});
