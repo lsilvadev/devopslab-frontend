296b4cda41a7503d674e992cfa95fb39534e2e45
Gabryelle Silva Santos <gabryelle-gaby@hotmail.com>
2023-05-02 12:08:57 -0300

    feat: add form
    Resolves: add-modal

--- src/components/modals/modalAddSong/modal-add-song.component.jsx
+++ src/components/modals/modalAddSong/modal-add-song.component.jsx
@@ -2,8 +2,12 @@ import React, { useEffect, useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { Modal, Form, message } from 'antd';
 
+import { GENRES_TYPES } from '../../../defaults/genres';
+import { getRuleFormItem } from '../../../utility/getRuleFormItem';
 import { 
   Title,
+  Input, 
+  Select,
 } from './modal-add-component.style';
 function ModalAddSong({ isModalOpen, handleCancel, setSongs }) {
   const [loading, setLoading] = useState(false);
@@ -15,12 +19,36 @@ function ModalAddSong({ isModalOpen, handleCancel, setSongs }) {
       onCancel={handleCancel}
     >
       <Title>Cadastrar música</Title>
+      <Form form={form} onFinish={onFinish}>
+        <Form.Item 
+          name="artist" 
+          rules={[getRuleFormItem('preencha o nome do artista')]}
+        >
+          <Input placeholder="Nome do artista" />
+        </Form.Item>
+        
+        <Form.Item 
+          name="title" 
+          rules={[getRuleFormItem('preencha o nome da música')]}
+        >
+          <Input placeholder="Nome da música" />
+        </Form.Item>
+
+        <Form.Item 
+          name="genre" 
+          rules={[getRuleFormItem('selecione o gênero')]}
+        >
+          <Select placeholder="Gênero" options={GENRES_TYPES} />
+        </Form.Item>
+      </Form>
     </Modal>
   );
 };
+
 ModalAddSong.propTypes = {
   isModalOpen: PropTypes.bool.isRequired,
   handleCancel: PropTypes.func.isRequired,
   setSongs: PropTypes.func.isRequired,
 };
+
 export default ModalAddSong;
