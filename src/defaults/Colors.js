27c4e791f82891178dadda846c6854f1e5c08af3
Gabryelle Silva Santos <gabryelle-gaby@hotmail.com>
2023-05-02 12:14:40 -0300

    feat: add colors and genres types
    Resolves: add-modal

--- src/defaults/Colors.js
+++ src/defaults/Colors.js
@@ -1,5 +1,8 @@
 export const PRIMARY_COLOR= '#FF637D';
-export const WHITE = '#ffffff';
 export const DARK_PRIMARY_COLOR = '#FF4F6C';
 
+export const WHITE = '#ffffff';
+export const COLOR_BG_CONTAINER = '#F8F8F8';
+
 export const FONT_COLOR = '#353D58';
+export const FONT_DARK_COLOR = '#9CA6CB';
